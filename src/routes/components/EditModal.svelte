22d486fd39cee384ae991d4dda71f9447c6453df
rollydelosreyes02 <rolly.a.delosreyes@gmail.com>
2024-07-01 17:52:05 +0800

    Removed view modal

---
+++ src/routes/components/EditModal.svelte
@@ -0,0 +1,11 @@
+<script>
+	import { Button, Modal } from 'flowbite-svelte';
+	export let defaultModal = true;
+    export let todo;
+</script>
+
+<Modal title={todo.title} bind:open={defaultModal} autoclose outsideclose>
+	<p class="text-base leading-relaxed text-gray-500 dark:text-gray-400">
+		{todo.description}
+	</p>
+</Modal>
--- src/routes/components/Todo.svelte
+++ src/routes/components/Todo.svelte
@@ -1,8 +1,8 @@
 <script lang="ts">
 	import { Card } from 'flowbite-svelte';
-	import ViewModal from './ViewModal.svelte';
 	let defaultModal = false;
 	import { EyeSlashOutline, EditOutline, TrashBinOutline }from 'flowbite-svelte-icons';
+	import EditModal from './EditModal.svelte';
 	const toggleModal = () => {
 		defaultModal = !defaultModal;
 	};
@@ -14,11 +14,9 @@
 		<h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{todo.title}</h5>
 		<p class="font-normal leading-tight text-gray-700 dark:text-gray-400">{todo.description}</p>
 		<div class="float-right">
-			<ViewModal {defaultModal} />
-			<button on:click={toggleModal}><EyeSlashOutline color="blue" size="xl" /></button>
-			<ViewModal {defaultModal} />
+			<EditModal {defaultModal} {todo}/>
 			<button on:click={toggleModal}><EditOutline color="yellow" size="xl" /></button>
-			<ViewModal {defaultModal} />
+			<EditModal {defaultModal} {todo}/>
 			<button on:click={toggleModal}><TrashBinOutline color="red" size="xl" /></button>
 		</div>
 	</Card>
